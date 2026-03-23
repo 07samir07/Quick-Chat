66cd60dc241328dc971981e774cfdab590f3a2f7
07samir07 <samirbirua6738@gmail.com>
2026-03-23 12:02:36 +0530

    Add chat area to ChatContainer with dummy message data display

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { messagesDummyData } from "../assets/assets";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
+      {/*------HEADER */}
+
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
@@ -17,6 +19,31 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
           className="md:hidden  max-w-7"
         />
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+
+      {/*-------CHAT AREA */}
+
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"}`}
+          >
+            {msg.image ? (
+              <img
+                src={assets.image}
+                alt=""
+                className="max-w-57.5 border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-50 md:text:sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === "680f50e4f10f3cd28382ecf9" ? "rounded-br-none" : "rounded-bl-none"}`}
+              >
+                {msg.text}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   ) : (
