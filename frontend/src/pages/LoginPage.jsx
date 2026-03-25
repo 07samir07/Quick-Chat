e9d02aa1c811481de8906feed5f65324fceb7f41
07samir07 <samirbirua6738@gmail.com>
2026-03-25 09:18:09 +0530

    Refactor RightSideBar button class for gradient background and update LoginPage with form submission handling and bio input

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import assets from "../assets/assets";
+import assets from "../assets/assets.js";
 
 const LoginPage = () => {
   const [currState, setCurrentState] = useState("Sign up");
@@ -9,15 +9,32 @@ const LoginPage = () => {
   const [bio, setBio] = useState();
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+  };
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/*LEFT*/}
       <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)] " />
       {/*RIGHT*/}
-      <form className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+      <form
+        onSubmit={onSubmitHandler}
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currState}
-          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+          {isDataSubmitted && (
+            <img
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
         </h2>
         {currState === "Sign up" && !isDataSubmitted && (
           <input
@@ -51,6 +68,54 @@ const LoginPage = () => {
             />
           </>
         )}
+        {currState === "Sign up" && isDataSubmitted && (
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-indigo-500"
+            placeholder="Provide a short bio..."
+            required
+          ></textarea>
+        )}
+        <button
+          type="submit"
+          className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+        >
+          {currState === "Sign up" ? "Create Account" : "Login Now"}
+        </button>
+
+        <div className="flex items-center gap-2 text-sm text-gray-500">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          {currState === "Sign up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account?{" "}
+              <span
+                className="font-medium text-violet-500 cursor-pointer"
+                onClick={() => {
+                  setCurrentState("Login");
+                  setIsDataSubmitted(false);
+                }}
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600">
+              Create an account{" "}
+              <span
+                className="font-medium text-violet-500 cursor-pointer"
+                onClick={() => setCurrentState("Sign up")}
+              >
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
