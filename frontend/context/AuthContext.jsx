8198a88f28934cef39414971728b6dbd5937304e
07samir07 <samirbirua6738@gmail.com>
2026-03-25 19:55:14 +0530

    installed dependencies for integrating frontend and backend

---
+++ frontend/context/AuthContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { io } from "socket.io-client";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const [authUser, setAuthUser] = useState(null);
+
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  //CHECK IF USERR IS AUTHENTICATED AND IF SO, SET THE USER DATA AND CONNECT THE SOCKET
+
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/check");
+      if (data.success) {
+        setAuthUser(data.user);
+        connectSocket(data.user);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  //CONNECT SOCKET FUNCTION TO HANDLE SOCKET CONNECTTION AND ONLINE USERS UPDATES
+  const connectSocket = (userData) => {
+    if (!userData || socket?.connected) return;
+    const newSocket = io(backendUrl, {
+      query: {
+        userId: userData._id,
+      },
+    });
+    newSocket.connect();
+    setSocket(newSocket);
+
+    newSocket.on("getOnlineUsers", () => {
+      setOnlineUsers(userIds);
+    });
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["token"] = token;
+    }
+    checkAuth();
+  }, []);
+
+  const value = {
+    axios,
+    authUser,
+    onlineUsers,
+    socket,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
